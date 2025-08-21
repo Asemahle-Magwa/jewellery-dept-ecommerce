71776a1c88e0aa2cfe8828bfd1affc2cf48904e0
Junior Mageza <tlangijunior212@gmail.com>
2025-08-21 12:10:46 +0200

    controllersTests Update

--- src/main/java/za/co/jewellerysystem/JewellerySystemApplication.java
+++ src/main/java/za/co/jewellerysystem/JewellerySystemApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class JewellerySystemApplication{
     public static void main(String[] args) {
         SpringApplication.run(JewellerySystemApplication.class, args);
+        //@G8_DevOps
     }
 }
